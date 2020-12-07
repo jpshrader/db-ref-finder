f623ac6d16e07dc3498736f349c55266ff487351
jpshrader <46694288+jpshrader@users.noreply.github.com>
2020-12-06 23:23:01 -0500

    Update SqlProcInspector.cs

--- SqlProcInspector.cs
+++ SqlProcInspector.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace DbRefFinder {
 	public static class SqlProcInspector {
 		public static void Inspect(DbAccess dbAccess, ConcurrentDictionary<string, ReferenceList> referenceMap) {
+			Console.WriteLine("Processing Database References...");
 			Parallel.ForEach(referenceMap.Keys, entity => {
 				var referenceList = referenceMap[entity];
 				var referencingProcs = dbAccess.FindRefsFromStoredProcs(entity);
